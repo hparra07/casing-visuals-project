1cf303597058a7c6d65c0a808f5a30e2b7f91f4a
hparra07 <howard.parra95@gmail.com>
2023-03-27 14:42:51 -0300

    addign animations

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,3 +21,31 @@ document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', 
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
 }))
+
+const observer = new IntersectionObserver(entries =>{
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            document.querySelectorAll('.services')[0].classList.add('fadeInTop');
+            document.querySelectorAll('.services')[1].classList.add('fadeInLeft');
+            document.querySelectorAll('.services')[2].classList.add('fadeInRight');
+            document.querySelectorAll('.services')[3].classList.add('fadeInLeft');
+            document.querySelectorAll('.services')[4].classList.add('fadeInRight');
+        }
+    })
+})
+
+observer.observe(document.querySelector('.what-we-do-services'))
+
+const observerAbout = new IntersectionObserver(entries =>{
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            document.querySelectorAll('.aboutAnimation')[0].classList.add('fadeInTop');
+            document.querySelectorAll('.aboutAnimation')[1].classList.add('fadeInLeft');
+            document.querySelectorAll('.aboutAnimation')[2].classList.add('fadeInLeft');
+            document.querySelectorAll('.aboutAnimation')[3].classList.add('fadeInTop');
+            document.querySelectorAll('.aboutAnimation')[4].classList.add('fadeInTop');
+        }
+    })
+})
+
+observerAbout.observe(document.querySelector('.about'))
